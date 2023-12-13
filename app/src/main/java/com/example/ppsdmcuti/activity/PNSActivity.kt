672f397c8808613d59d402f30e3354a002aa49ff
mawanikhsani <mawanikhsani99@gmail.com>
2023-12-13 21:08:44 +0700

    Perbaikan PNS activity

--- app/src/main/java/com/example/ppsdmcuti/activity/PNSActivity.kt
+++ app/src/main/java/com/example/ppsdmcuti/activity/PNSActivity.kt
@@ -1,6 +1,5 @@
 package com.example.ppsdmcuti.activity
 
-import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,112 +8,186 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.example.ppsdmcuti.R
 import java.util.Calendar
 
 class PNSActivity : AppCompatActivity() {
 
-    val divisi = arrayOf("Fungsional", "Struktural", "Bagian Perencanaan", "Rumah Tangga dan Sarana Pra Sarana",
-        "Administrasi dan kepegawaian")
-    val atasan = arrayOf("Nama Atasan 1", "Nama Atasan 2")
-    val jnsCuti = arrayOf("Cuti Tahunan", "Cuti Sakit")
-    private lateinit var btnTglAwal: Button
-    private lateinit var btnTglAkhir: Button
-    private var selectedStartDate : String? = null
-    private var selectedEndDate : String? = null
-    private val PICK_FILE_REQUEST_CODE = 1 // Request code for file picker
+    // Inisialisasi view
+    private lateinit var ivBackHome: ImageView
+    private lateinit var tvNama: TextView
+    private lateinit var etNama: EditText
+    private lateinit var tvNIP: TextView
+    private lateinit var etNIP: EditText
+    private lateinit var tvDivisi: TextView
+    private lateinit var spDiv: Spinner
+    private lateinit var tvAtasan: TextView
+    private lateinit var spAtasan: Spinner
+    private lateinit var tvJenisCuti: TextView
+    private lateinit var spJenisCuti: Spinner
+    private lateinit var tvTglAwalCuti: TextView
+    private lateinit var etTglAwal: EditText
+    private lateinit var btnTglAwal: ImageButton
+    private lateinit var tvTglAkhir: TextView
+    private lateinit var etTglAkhir: EditText
+    private lateinit var btnTglAkhir: ImageButton
+    private lateinit var tvAlamat: TextView
+    private lateinit var etAlamat: EditText
+    private lateinit var tvUpDoc: TextView
+    private lateinit var btnUpDoc: Button
+    private lateinit var tvAlasan: TextView
+    private lateinit var etAlasan: EditText
+    private lateinit var btnAjukan: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pnsactivity)
 
-        //inisialisasi button
+        // Inisialisasi view
+        ivBackHome = findViewById(R.id.ivBackHome)
+        tvNama = findViewById(R.id.tvNama)
+        etNama = findViewById(R.id.etNama)
+        tvNIP = findViewById(R.id.tvNIP)
+        etNIP = findViewById(R.id.etNIP)
+        tvDivisi = findViewById(R.id.tvDivisi)
+        spDiv = findViewById(R.id.spDiv)
+        tvAtasan = findViewById(R.id.tvAtasan)
+        spAtasan = findViewById(R.id.spAtasan)
+        tvJenisCuti = findViewById(R.id.tvJenisCuti)
+        spJenisCuti = findViewById(R.id.spJenisCuti)
+        tvTglAwalCuti = findViewById(R.id.tvTglAwalCuti)
+        etTglAwal = findViewById(R.id.etTglAwal)
         btnTglAwal = findViewById(R.id.btnTglAwal)
-        btnTglAkhir = findViewById(R.id.iBtntglAkhir)
-        val uploadButton: Button = findViewById(R.id.btnUpDoc)
+        tvTglAkhir = findViewById(R.id.tvTglAkhir)
+        etTglAkhir = findViewById(R.id.etTglAkhir)
+        btnTglAkhir = findViewById(R.id.btnTglAkhir)
+        tvAlamat = findViewById(R.id.tvAlamat)
+        etAlamat = findViewById(R.id.etAlamat)
+        tvUpDoc = findViewById(R.id.tvUpDoc)
+        btnUpDoc = findViewById(R.id.btnUpDoc)
+        tvAlasan = findViewById(R.id.tvAlasan)
+        etAlasan = findViewById(R.id.etAlasan)
+        btnAjukan = findViewById(R.id.btnAjukan)
 
-        btnTglAwal.setOnClickListener{
-            showDatePickerDialog(true)
-        }
-        btnTglAkhir.setOnClickListener{
-            showDatePickerDialog(false)
-        }
-        uploadButton.setOnClickListener {
-            // Open file picker when the button is clicked
-            openFilePicker()
+        // Set listener untuk ivBackHome
+        ivBackHome.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
         }
 
-        //spinner divisi
-        val div= findViewById<Spinner>(R.id.spDiv)
-        val divAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, divisi)
-        div.adapter = divAdapter
-        div.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                Toast.makeText(applicationContext, "Anda memilih divisi " + div[position], Toast.LENGTH_SHORT).show()
+        // Set listener untuk etNama
+        etNama.setOnClickListener {
+
+        }
+
+        // Set listener untuk etNIP
+        etNIP.setOnClickListener {
+
+        }
+
+        // Set listener untuk etAlamat
+        etAlamat.setOnClickListener {
+
+        }
+
+        // Set listener untuk etAlasan
+        etAlasan.setOnClickListener {
+
+        }
+
+        // Set listener untuk btnAjukan
+        btnAjukan.setOnClickListener {
+            // Menampilkan pemberitahuan "Berhasil mengajukan"
+            Toast.makeText(this, "Berhasil mengajukan", Toast.LENGTH_SHORT).show()
+        }
+
+        // Inisialisasi data untuk Spinner
+        val divisiOptions = arrayOf("Fungsional", "Struktural", "Bagian Perencanaan", "Rumah Tangga dan Sarana Pra Sarana", "Administrasi dan kepegawaian")
+
+        // Buat ArrayAdapter menggunakan divisiOptions dan layout bawaan untuk Spinner
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, divisiOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spDiv.adapter = adapter
+
+        // Inisialisasi data untuk Spinner Atasan
+        val atasanOptions = arrayOf("Nama Atasan 1", "Nama Atasan 2")
+
+        // Buat ArrayAdapter menggunakan atasanOptions dan layout bawaan untuk Spinner
+        val atasanAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, atasanOptions)
+        atasanAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spAtasan.adapter = atasanAdapter
+
+
+        // Inisialisasi data untuk Spinner Jenis Cuti
+        val jenisCutiOptions = arrayOf("Cuti Tahunan", "Cuti Sakit", "Cuti Karena Alasan Penting", "Cuti Besar", "Cuti Melahirkan", "Cuti diluar Tanggungan Negara")
+
+        // Buat ArrayAdapter menggunakan jenisCutiOptions dan layout bawaan untuk Spinner
+        val jenisCutiAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, jenisCutiOptions)
+        jenisCutiAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spJenisCuti.adapter = jenisCutiAdapter
+
+        // Set listener untuk spDiv
+        spDiv.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Menampilkan pilihan yang dipilih
+                //Toast.makeText(this@PNSActivity, "Selected: ${divisiOptions[position]}", Toast.LENGTH_SHORT).show()
+                tvDivisi.text = "Jenis Cuti: ${divisiOptions[position]}"
+                tvDivisi.setTextColor(ContextCompat.getColor(this@PNSActivity, android.R.color.black))
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
             }
-
         }
 
-        //spinner atasan
-        val ketua = findViewById<Spinner>(R.id.spAtasan)
-        val ketuaAdapter  = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, atasan)
-        ketua.adapter = ketuaAdapter
-        ketua.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                Toast.makeText(applicationContext, "Atasan anda adalah " + ketua[position], Toast.LENGTH_SHORT).show()
+        // Set listener untuk spAtasan
+        spAtasan.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Menampilkan pilihan yang dipilih dari Spinner Atasan
+                //Toast.makeText(this@PNSActivity, "Selected Atasan: ${atasanOptions[position]}", Toast.LENGTH_SHORT).show()
+                tvAtasan.text = "Jenis Cuti: ${atasanOptions[position]}"
+                tvAtasan.setTextColor(ContextCompat.getColor(this@PNSActivity, android.R.color.black))
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
             }
-
         }
 
-        //spinner jenis cuti
-        val cuti = findViewById<Spinner>(R.id.spJenisCuti)
-        val cutiAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, jnsCuti)
-        cuti.adapter = cutiAdapter
-        cuti.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                Toast.makeText(applicationContext, "Anda akan mengambil " + cuti[position], Toast.LENGTH_SHORT).show()
+        // Set listener untuk spJenisCuti
+        spJenisCuti.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Menampilkan pilihan yang dipilih dari Spinner Jenis Cuti
+                tvJenisCuti.text = "Jenis Cuti: ${jenisCutiOptions[position]}"
+                tvJenisCuti.setTextColor(ContextCompat.getColor(this@PNSActivity, android.R.color.black))
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
             }
-
         }
+
+        // Set listener untuk btnTglAwal
+        btnTglAwal.setOnClickListener {
+            // Tampilkan DatePickerDialog untuk memilih tanggal awal
+            showDatePickerDialogAwal(etTglAwal)
+        }
+
+        // Set listener untuk btnTglAkhir
+        btnTglAkhir.setOnClickListener {
+            // Tampilkan DatePickerDialog untuk memilih tanggal akhir
+            showDatePickerDialogAkhir(etTglAkhir)
+        }
+
     }
 
-    private fun openFilePicker() {
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "*/*" // Allow all file types
-        startActivityForResult(intent, PICK_FILE_REQUEST_CODE)
-    }
-
-    private fun showDatePickerDialog(isStartDate: Boolean) {
+    private fun showDatePickerDialogAwal(etTglAwal: EditText) {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
@@ -122,32 +195,33 @@ class PNSActivity : AppCompatActivity() {
 
         val datePickerDialog = DatePickerDialog(
             this,
-            { _, year, month, day ->
-                val selectedDate = "$year-${month + 1}-${day}"
-
-                if (isStartDate) {
-                    selectedStartDate = selectedDate
-                    btnTglAwal.text = selectedStartDate
-                } else {
-                    selectedEndDate = selectedDate
-                    btnTglAkhir.text = selectedEndDate
-                }
+            { _, selectedYear, selectedMonth, selectedDay ->
+                // Menetapkan tanggal yang dipilih ke dalam etTglAwal
+                this.etTglAwal.setText("$selectedDay-${selectedMonth + 1}-$selectedYear")
             },
-            year,
-            month,
-            day
+            year, month, day
         )
+
+        // Tampilkan DatePickerDialog
         datePickerDialog.show()
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == PICK_FILE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            data?.data?.let { fileUri ->
-                // Handle the selected file URI, you can use it to upload the file
-                val selectedFilePath = fileUri.path
-                // Do something with the selected file path
-            }
-        }
+    private fun showDatePickerDialogAkhir(etTglAkhir: EditText) {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, selectedYear, selectedMonth, selectedDay ->
+                // Menetapkan tanggal yang dipilih ke dalam etTglAkhir
+                this.etTglAkhir.setText("$selectedDay-${selectedMonth + 1}-$selectedYear")
+            },
+            year, month, day
+        )
+
+        // Tampilkan DatePickerDialog
+        datePickerDialog.show()
     }
 }
--- app/src/main/java/com/example/ppsdmcuti/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/ppsdmcuti/fragment/ProfileFragment.kt
@@ -1,20 +1,30 @@
 package com.example.ppsdmcuti.fragment
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.cardview.widget.CardView
 import com.example.ppsdmcuti.R
 import com.example.ppsdmcuti.activity.LoginActivity
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileFragment : Fragment() {
 
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var ivProfileUser: CircleImageView
+    private lateinit var ivUbahFoto: ImageView
+
+    private val PICK_IMAGE_REQUEST = 1
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,8 +32,11 @@ class ProfileFragment : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        ivProfileUser = view.findViewById(R.id.ivProfileUser)
+        ivUbahFoto = view.findViewById(R.id.ivUbahFoto)
 
-        sharedPreferences = requireContext().getSharedPreferences("loginPrefs", Context.MODE_PRIVATE)
+        sharedPreferences =
+            requireContext().getSharedPreferences("loginPrefs", Context.MODE_PRIVATE)
 
         val btnLogOut: CardView = view.findViewById(R.id.btnLogOut)
 
@@ -40,5 +53,38 @@ class ProfileFragment : Fragment() {
         }
 
         return view
+
+//        // Load saved image URI
+//        val imageUriString = sharedPreferences.getString("imageUri", null)
+//        if (imageUriString != null) {
+//            val imageUri = Uri.parse(imageUriString)
+//            ivProfileUser.setImageURI(imageUri)
+//        }
+//
+//        ivUbahFoto.setOnClickListener {
+//            // Membuka galeri untuk memilih gambar
+//            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//            startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST)
+//        }
+//
+//        return view
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+//            // Mendapatkan URI gambar yang dipilih dari galeri
+//            val imageUri: Uri = data.data!!
+//
+//            // Menyimpan URI gambar ke SharedPreferences
+//            val editor = sharedPreferences.edit()
+//            editor.putString("imageUri", imageUri.toString())
+//            editor.apply()
+//
+//            // Menampilkan gambar pada ImageView
+//            ivProfileUser.setImageURI(imageUri)
+//        }
+//    }
     }
 }
